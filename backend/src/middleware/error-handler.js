f95a48927ca8e30339d6af9c5463f8b347d19487
kebaL32 <kebronluel813@gmail.com>
2026-07-06 07:19:20 -0700

    updated

--- backend/src/middleware/error-handler.js
+++ backend/src/middleware/error-handler.js
@@ -1,6 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 
 export const errorHandler = (err, req, res, next) => {
+  console.error("=== ERROR HANDLER ===");
+  console.error(err);
+
   const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong",
@@ -8,7 +11,6 @@ export const errorHandler = (err, req, res, next) => {
 
   if (err.code === "ER_DUP_ENTRY") {
     customError.statusCode = StatusCodes.BAD_REQUEST;
-
     customError.msg = "Duplicate value entered";
   }
 
